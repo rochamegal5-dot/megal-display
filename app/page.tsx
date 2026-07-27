f29d766c30c02e1752333e6f5f8557ca4dac9c28
MEGALROCHA <rochamegal5@gmail.com>
2026-07-27 16:02:33 -0300

    Refactor Home component layout and add hero section
    Updated the Home component layout with a hero section and additional headings.

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,19 @@
 export default function Home() {
   return (
-    <main>
-      <h1>Megal Display</h1>
+    <main className="container">
+      <div className="hero">
+
+        <h1>Megal Display</h1>
+
+        <h2>
+          Quiniela · Tómbola · 5 de Oro
+        </h2>
+
+        <p>
+          Proyecto en construcción
+        </p>
+
+      </div>
     </main>
   );
 }
