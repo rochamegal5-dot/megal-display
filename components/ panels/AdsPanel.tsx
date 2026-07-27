b93575feeb9ba697d5ad33a7ae1a1956c7abea47
MEGALROCHA <rochamegal5@gmail.com>
2026-07-27 16:25:00 -0300

    Update AdsPanel.tsx

--- components/ panels/AdsPanel.tsx
+++ components/ panels/AdsPanel.tsx
@@ -1,1 +1,9 @@
+export default function AdsPanel() {
+  return (
+    <footer className="h-40 bg-green-700 flex items-center justify-center text-3xl font-bold">
 
+      ESPACIO PUBLICITARIO MEGAL ROCHA
+
+    </footer>
+  );
+}
