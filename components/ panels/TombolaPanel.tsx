d80d0a97f2e5ba27af5fdf7fbf8acb716dc41d2a
MEGALROCHA <rochamegal5@gmail.com>
2026-07-27 16:29:25 -0300

    Update TombolaPanel.tsx

--- components/ panels/TombolaPanel.tsx
+++ components/ panels/TombolaPanel.tsx
@@ -1,13 +1,11 @@
-export default function TombolaPanel() {
+eexport default function TombolaPanel() {
   return (
     <section className="rounded-xl bg-slate-900 p-6">
-
-      <h2 className="text-2xl font-bold text-green-400 mb-4">
+      <h2 className="text-2xl font-bold text-green-400">
         Tómbola
       </h2>
 
       <p>Esperando resultados...</p>
-
     </section>
   );
 }
