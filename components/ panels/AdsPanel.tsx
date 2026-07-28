75ef84791af420a85b548e57c249e39c5b4cd0a2
MEGALROCHA <rochamegal5@gmail.com>
2026-07-27 23:04:18 -0300

    Delete components/ panels directory

--- components/ panels/AdsPanel.tsx
+++
@@ -1,7 +0,0 @@
-export default function AdsPanel() {
-  return (
-    <footer className="h-32 rounded-xl bg-green-700 flex items-center justify-center">
-      ESPACIO PUBLICITARIO MEGAL ROCHA
-    </footer>
-  );
-}
