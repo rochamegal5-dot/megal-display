8e5bf7cba50b59489ca2f14f8940c542acaf02d0
MEGALROCHA <rochamegal5@gmail.com>
2026-07-27 22:47:30 -0300

    Update import paths for components in page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
-import Header from "@/components/layout/Header";
-import QuinielaPanel from "@/components/panels/QuinielaPanel";
-import TombolaPanel from "@/components/panels/TombolaPanel";
-import FiveGoldPanel from "@/components/panels/FiveGoldPanel";
-import AdsPanel from "@/components/panels/AdsPanel";
+import Header from "../components/layout/Header";
+import QuinielaPanel from "../components/panels/QuinielaPanel";
+import TombolaPanel from "../components/panels/TombolaPanel";
+import FiveGoldPanel from "../components/panels/FiveGoldPanel";
+import AdsPanel from "../components/panels/AdsPanel";
 
 export default function Home() {
   return (
