87e48f5af9b9d816181f223bdb4ea3bbe568e494
MEGALROCHA <rochamegal5@gmail.com>
2026-07-28 00:02:50 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,3 @@
 export default function Home() {
-  return (
-    <main
-      style={{
-        background: "black",
-        color: "lime",
-        height: "100vh",
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-        fontSize: "70px",
-        fontWeight: "bold",
-      }}
-    >
-      MEGAL ROCHA FUNCIONA
-    </main>
-  );
+  throw new Error("ESTA ES UNA PRUEBA");
 }
