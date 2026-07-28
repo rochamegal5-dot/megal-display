24f7011b4fb4e2f3b64fc18981f78a6bbcc1ddbd
MEGALROCHA <rochamegal5@gmail.com>
2026-07-28 17:18:08 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,14 @@
 import Header from "@/components/layout/Header";
 import QuinielaPanel from "@/components/panels/QuinielaPanel";
-import TombolaPanel from "@/components/panels/TombolaPanel";
-import FiveGoldPanel from "@/components/panels/FiveGoldPanel";
-import AdsPanel from "@/components/panels/AdsPanel";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-slate-950 text-white">
-
       <Header />
 
-      <section className="grid grid-cols-3 gap-6 p-6">
-
+      <section className="p-6">
         <QuinielaPanel />
-
-        <TombolaPanel />
-
-        <FiveGoldPanel />
-
       </section>
-
-      <AdsPanel />
-
     </main>
   );
 }
