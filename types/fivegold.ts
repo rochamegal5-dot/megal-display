67a65c2c69be4d4c97fb8a07fe7c881af30f49c7
MEGALROCHA <rochamegal5@gmail.com>
2026-07-29 00:00:23 -0300

    Add FiveGoldSorteo and FiveGoldResponse interfaces

--- types/fivegold.ts
+++ types/fivegold.ts
@@ -1,1 +1,16 @@
+export interface FiveGoldSorteo {
+  bolillas: number[];
+  bolillaExtra: number;
+  revancha: number[];
 
+  pozoDeOro: string;
+  pozoRevancha: string;
+  pozoDePlata: string;
+}
+
+export interface FiveGoldResponse {
+  fecha: string;
+  sorteo: FiveGoldSorteo;
+  ultimaActualizacion: string;
+  estado?: string;
+}
