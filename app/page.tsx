eaef65f84b4a1be0d6bb722065175ff77bb78995
MEGALROCHA <rochamegal5@gmail.com>
2026-07-28 22:37:08 -0300

    Update text and change div to main in Home component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
 export default function Home() {
   return (
-    <div className="min-h-screen bg-red-600 flex items-center justify-center">
+    <main className="min-h-screen bg-red-600 flex items-center justify-center">
       <h1 className="text-7xl font-bold text-white">
-        TAILWIND OK
+        TAILWIND FUNCIONA
       </h1>
-    </div>
+    </main>
   );
 }
