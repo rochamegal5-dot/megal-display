62ed637a9239a25deeafe8f13b7c9776fc7f6c78
MEGALROCHA <rochamegal5@gmail.com>
2026-07-29 23:19:19 -0300

    Add Ball component with customizable props

---
+++ components/ui/Ball.tsx
@@ -0,0 +1,36 @@
+interface Props{
+
+numero:string
+
+color?:
+"gold"|
+"silver"|
+"green"|
+"red"
+
+size?:
+"sm"|
+"md"|
+"lg"
+
+}
+
+export default function Ball({
+
+numero,
+color="gold",
+size="md"
+
+}:Props){
+
+return(
+
+<div className={`ball ${color} ${size}`}>
+
+{numero}
+
+</div>
+
+)
+
+}
