b35a54ad1f2e65da8516c2c347d41873facb4ec0
MEGALROCHA <rochamegal5@gmail.com>
2026-07-30 10:09:20 -0300

    Update scraper.ts

--- services/scraper.ts
+++ services/scraper.ts
@@ -1,22 +1,14 @@
+import axios from "axios";
 import * as cheerio from "cheerio";
 
-export async function obtenerHTML(url: string) {
-  const res = await fetch(url, {
+export async function loadPage(url: string) {
+  const response = await axios.get(url, {
     headers: {
       "User-Agent":
         "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36",
     },
-    cache: "no-store",
+    timeout: 15000,
   });
 
-  if (!res.ok) {
-    throw new Error(`Error ${res.status}`);
-  }
-
-  return await res.text();
-}
-
-export async function cargarPagina(url: string) {
-  const html = await obtenerHTML(url);
-  return cheerio.load(html);
+  return cheerio.load(response.data);
 }
