d40d5e8272428cbdc5eddb639f3190b0fde652ab
MEGALROCHA <rochamegal5@gmail.com>
2026-07-30 00:37:53 -0300

    Add GET endpoint for media resources
    Implement GET endpoint to return media items in JSON format.

---
+++ app/api/media/route.ts
@@ -0,0 +1,33 @@
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  return NextResponse.json([
+    {
+      type: "video",
+      src: "/media/videos/promo1.mp4",
+    },
+    {
+      type: "image",
+      src: "/media/imagenes/imagen1.jpg",
+      duration: 8000,
+    },
+    {
+      type: "video",
+      src: "/media/videos/promo2.mp4",
+    },
+    {
+      type: "image",
+      src: "/media/imagenes/imagen2.jpg",
+      duration: 8000,
+    },
+    {
+      type: "video",
+      src: "/media/videos/promo3.mp4",
+    },
+    {
+      type: "image",
+      src: "/media/imagenes/imagen3.jpg",
+      duration: 8000,
+    },
+  ]);
+}
