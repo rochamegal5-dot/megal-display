50d00944ee5b45e0b01065b5f69e2d4a05fc8caa
MEGALROCHA <rochamegal5@gmail.com>
2026-07-31 00:03:56 -0300

    Update DNLQ URL and change return type

--- services/providers/dnlq.ts
+++ services/providers/dnlq.ts
@@ -1,24 +1,18 @@
 import axios from "axios";
-import * as cheerio from "cheerio";
 
 const URL =
-  "https://www.loteria.gub.uy/ver_resultados.php";
+  "https://www.loteria.gub.uy/movil/24_25_resultados.php";
 
-export async function loadDNLQ() {
-
+export async function loadDNLQ(): Promise<string> {
   const { data } = await axios.get(URL, {
-
     timeout: 15000,
-
     headers: {
-
       "User-Agent":
-        "Mozilla/5.0 MegalDisplay",
-
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) MegalDisplay/2.0",
+      Accept:
+        "text/html,application/xhtml+xml",
     },
-
   });
 
-  return cheerio.load(data);
-
+  return String(data);
 }
