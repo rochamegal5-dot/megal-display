205fe0829d00a2d2b65dec40419fdba8c7a3e295
MEGALROCHA <rochamegal5@gmail.com>
2026-07-30 23:56:57 -0300

    Create parseQuiniela function in quiniela.ts

---
+++ services/parsers/quiniela.ts
@@ -0,0 +1,17 @@
+import type { CheerioAPI } from "cheerio";
+
+export function parseQuiniela($: CheerioAPI) {
+
+  // aquí irá el parser oficial
+
+  return {
+
+    fecha: "",
+
+    vespertina: [],
+
+    nocturna: [],
+
+  };
+
+}
