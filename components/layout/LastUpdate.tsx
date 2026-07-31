2b86ce67026233a454a434ecd02be4c92a613437
MEGALROCHA <rochamegal5@gmail.com>
2026-07-31 00:31:31 -0300

    Add LastUpdate component with real-time clock
    Implement a LastUpdate component that displays the current time in 'es-UY' locale, updating every second.

--- components/layout/LastUpdate.tsx
+++ components/layout/LastUpdate.tsx
@@ -1,1 +1,37 @@
+'use client'
 
+import { useEffect, useState } from 'react'
+
+export default function LastUpdate(){
+
+const[now,setNow]=useState("")
+
+useEffect(()=>{
+
+const id=setInterval(()=>{
+
+setNow(
+
+new Date().toLocaleTimeString("es-UY")
+
+)
+
+},1000)
+
+return()=>clearInterval(id)
+
+},[])
+
+return(
+
+<div className="last-update">
+
+🟢 Última actualización
+
+{now}
+
+</div>
+
+)
+
+}
