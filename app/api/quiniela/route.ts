49dd9c2b1817a6ecc6bd38984824947ff710fcb3
MEGALROCHA <rochamegal5@gmail.com>
2026-08-03 16:32:56 -0300

    Add type annotation for datos in GET function

--- app/api/quiniela/route.ts
+++ app/api/quiniela/route.ts
@@ -5,14 +5,20 @@ export async function GET() {
 
   try {
 
-    const datos = await getResultados();
+    const datos: any = await getResultados();
 
     return NextResponse.json({
+
       fecha: datos.fecha,
+
       vespertina: datos.quiniela.vespertina,
+
       nocturna: datos.quiniela.nocturna,
+
       ultimaActualizacion: new Date(),
+
       estado: "OK"
+
     });
 
   } catch (error) {
