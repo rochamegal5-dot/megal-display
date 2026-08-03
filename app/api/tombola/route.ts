f2a0ccc174fd82cb368ab3b496fd29ba103d959b
MEGALROCHA <rochamegal5@gmail.com>
2026-08-03 16:33:59 -0300

    Refactor GET function to eliminate duplicate variable
    Removed duplicate declaration of 'datos' and fixed indentation.

--- app/api/tombola/route.ts
+++ app/api/tombola/route.ts
@@ -3,10 +3,9 @@ import { NextResponse } from "next/server";
 import { getResultados } from "@/services/providers/provider";
 
 export async function GET() {
+  const datos: any = await getResultados();
 
-  const datos = await getResultados();
-
-  return NextResponse.json({
+    return NextResponse.json({
 
     fecha: datos.fecha,
 
